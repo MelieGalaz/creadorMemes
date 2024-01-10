a04699ceee9963559c702f5b0919e9b1d5605c6a
melie galaz <galazmely27@gmail.com>
2024-01-10 14:11:22 -0300

    funcionalidad los botones para que el panel aparezca cuando esta en uso y desaparezca cuando se toca la X

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const modoOscuroBtn = document.getElementById('modoOscuroBtn');
 const body = document.body;
+const sectionImagen = document.querySelector(".sec-imagen");
+const sectionTexto = document.querySelector(".sec-texto");
+const panelAside = document.querySelector("#panel");
+
 
 modoOscuroBtn.addEventListener('click', () => {
     body.classList.toggle('modo-oscuro');
@@ -10,4 +14,24 @@ modoOscuroBtn.addEventListener('click', () => {
         modoOscuroBtn.innerHTML = '<i class="fa-regular fa-lightbulb"></i>Modo oscuro';
     }
 });
+function mostrarPanel(){
+document.getElementById("panel").style.display="block";
+}
+function ocultarPanel(){
+    document.getElementById("panel").style.display="none";
+    }
+
+document.querySelector(".imagen").addEventListener("click", (e) => {
+    sectionTexto.classList.add("oculto");
+    sectionImagen.classList.remove("oculto");
+    mostrarPanel();
+    e.preventDefault();
+})
+
+document.querySelector(".texto").addEventListener("click", (e) => {
+    sectionImagen.classList.add("oculto");
+    sectionTexto.classList.remove("oculto");
+    mostrarPanel();
+    e.preventDefault();
+})
 
