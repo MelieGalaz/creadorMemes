255715f2d7fa958e95fa3c8eba52c501ef81e1fe
melie galaz <galazmely27@gmail.com>
2024-01-15 22:14:05 -0300

    agregue estilos de modo oscuro a los paneles de aside y agregue comentarios para mayor organizacion

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const panelAside = document.querySelector("#panel");
 const divImagen = document.querySelector(".contenedorImagen")
 const cargarImagen = document.getElementById("cargarImagen")
 
+
+/********************modo oscuro************************ */
 modoOscuroBtn.addEventListener('click', () => {
     body.classList.toggle('modo-oscuro');
 
@@ -16,6 +18,9 @@ modoOscuroBtn.addEventListener('click', () => {
     }
 });
 
+
+
+/********************mostar paneles desde menu************** */
 function mostrarPanel() {
     document.getElementById("panel").style.display = "block";
 }
@@ -24,6 +29,8 @@ function ocultarPanel() {
     document.getElementById("panel").style.display = "none";
 }
 
+
+/*********************ocultar panel con el checkbox************ */
 document.querySelector(".imagen").addEventListener("click", (e) => {
     sectionTexto.classList.add("oculto");
     sectionImagen.classList.remove("oculto");
@@ -38,6 +45,8 @@ document.querySelector(".texto").addEventListener("click", (e) => {
     e.preventDefault();
 })
 
+/************************cargar fotos desde la url**************** */
+
 cargarImagen.addEventListener("change", (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -45,12 +54,17 @@ cargarImagen.addEventListener("change", (e) => {
         reader.onload = (e) => {
             const imgElement = document.createElement("img");
             imgElement.src = e.target.result;
+            imagenContainer.innerHTML = '';
             const divImagen = document.querySelector(".imagenInterna");
             divImagen.appendChild(imgElement);
         };
         reader.readAsDataURL(file);
     }
 });
+
+
+
+/**************************cargar foto desde compu******************** */
 function cargarFoto() {
     const urlInput = document.getElementById('urlInput').value;
     const imagenContainer = document.querySelector('.imagenInterna');
@@ -61,6 +75,10 @@ function cargarFoto() {
     imagenContainer.innerHTML = ''; 
     imagenContainer.appendChild(imagen);
     }
+
+
+
+/************elimina texto de imagen por defecto de la carga de compu*********** */
 document.addEventListener('DOMContentLoaded', function () {
         const fileInput = document.getElementById('cargarImagen');
         const customFileUpload = document.getElementById('customFileUpload');
